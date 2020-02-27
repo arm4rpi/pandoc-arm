361ee5c3d1b8cbea33cf843d3d86f13159b8170a
annprog <tecbbs@qq.com>
2020-02-28 03:30:16 +0800

    test chroot

---
+++ chroot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /root/rootfs
+mount -t devtmpfs devtmpfs dev
+mount -t devpts devpts dev/pts
+mount -t sysfs sysfs sys
+mount -t tmpfs tmpfs tmp
+mount -t proc proc proc
+
+chroot . ./init.sh
