0069c7f1ea5ea6b9455e3cac33a7a6f38604a6d5
annprog <tecbbs@qq.com>
2020-02-29 20:38:23 +0800

    test

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+apk update
+apk add git aria2 make libc-dev pcre-dev libc6-compat ncurses5-libs gmp-dev llvm zlib-dev gcc perl g++
+
+
+# numactl-dev need edge source. but others may conflict with edge. so delete it after install numactl-dev
+cat >/etc/apk/repositories<<EOF
+http://dl-cdn.alpinelinux.org/alpine/edge/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+EOF
+
+apk add numactl-dev
+ln -s /usr/lib/libncurses.so.5 /usr/lib/libtinfo.so.5
+
+sed -i '/edge/d' /etc/apk/repositories
+
+
+cd /root
+aria2c -x 16 https://github.com/commercialhaskell/ghc/releases/download/ghc-8.6.2-release/ghc-8.6.2-aarch64-deb8-linux.tar.xz && \
+	tar Jxvf ghc-8.6.2-aarch64-deb8-linux.tar.xz && \
+	rm -f ghc-8.6.2-aarch64-deb8-linux.tar.xz && \
+	cd ghc-8.6.2 && \
+	./configure && \
+	make install
+
+/root/stack.sh
+
+exit 0
--- chroot.sh
+++ chroot.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
 cd /root/rootfs
+
+cp /etc/resolv.conf etc
+apt-get install -y qemu-user-static
+cp /usr/bin/qemu-aarch64-static usr/bin
+
 mount -t devtmpfs devtmpfs dev
 mount -t devpts devpts dev/pts
 mount -t sysfs sysfs sys
 mount -t tmpfs tmpfs tmp
 mount -t proc proc proc
 
-ls -l init.sh
 chroot . ./init.sh
