c553ec542b769f80a7ec47b49cccae4c4d59f761
annprog <tecbbs@qq.com>
2020-03-03 01:14:21 +0800

    checkip

--- chroot.sh
+++ chroot.sh
@@ -6,6 +6,13 @@ cp build.sh /ghc/rootfs
 cd /ghc/rootfs
 
 cp /etc/resolv.conf etc
+
+CODE=`curl -s http://ip-api.com/json |tr ',' '\n' |grep "countryCode" |awk -F'"' '{print $4}'`
+
+if [ "$CODE"x == "CN"x ];then
+	sed -i -r 's/deb.debian.org|security.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
+fi
+
 apt-get update
 apt-get install -y qemu-user-static
 cp /usr/bin/qemu-aarch64-static usr/bin
