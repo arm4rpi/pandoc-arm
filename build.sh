9b24cb16c411b878156e9c017f6694df042f86c1
annprog <tecbbs@qq.com>
2020-03-02 19:23:16 +0800

    [ci skip]support release

--- build.sh
+++ build.sh
@@ -3,19 +3,64 @@ set -e
 
 ARCH=`arch`
 
+function getTag() {
+	git describe --tags |awk -F'-' '{print $1}'
+}
+
+function release() {
+	DIR=/root/.local/bin
+	mv $DIR/$1 $DIR/$1-$2-$ARCH
+	xz $DIR/$1-$2-$ARCH
+}
+
 apt-get update
+apt-get install -y xz-utils
 
 cd /root
 /root/stack.sh
 
+# pandoc
 git clone https://github.com/jgm/pandoc
 cd pandoc
+tag=`getTag`
+git checkout $tag
+sed -i 's/^resolver.*/resolver: lts-14.6/' stack.yaml
 cat >> stack.yaml <<EOF
 system-ghc: true
 arch: $ARCH
 EOF
 
-
 stack install -v --flag 'pandoc:static'
+release pandoc
+
+# pandoc-citeproc
+cd ../
+git clone https://github.com/jgm/pandoc-citeproc
+cd pandoc-citeproc
+tag=`getTag`
+git checkout $tag
+sed -i 's/^resolver.*/resolver: lts-14.6/' stack.yaml
+cat >> stack.yaml <<EOF
+system-ghc: true
+arch: $ARCH
+EOF
+
+stack install -v --flag 'pandoc-citeproc:static'
+release "pandoc-citeproc"
+
+# pandoc-crossref
+cd ../
+git clone https://github.com/lierdakil/pandoc-crossref
+cd pandoc-crossref
+tag=`getTag`
+git checkout $tag
+sed -i 's/^resolver.*/resolver: lts-14.6/' stack.yaml
+cat >> stack.yaml <<EOF
+system-ghc: true
+arch: $ARCH
+EOF
+
+stack install -v
+release "pandoc-crossref"
 
 exit $?
