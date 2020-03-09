e264a5541782f7633740ba6b545ac5b5a827741f
annprog <tecbbs@qq.com>
2020-03-09 18:10:45 +0800

    test

--- build.sh
+++ build.sh
@@ -3,34 +3,19 @@
 set -e
 
 ARCH=`arch`
-BINDIR=/root/bin
+PKG=`basename $0`
+CABALDIR="/home/runner/.cabal"
 
 apt-get update
-apt-get install -y cabal-install git curl aria2
-
-[ ! -d $BINDIR ] && mkdir $BINDIR
-
-function getTag() {
-	cd $1
-	TAG=`git describe --tags`
-	cd ../
-	echo $TAG
-}
-
-function release() {
-	mv $BINDIR/$1 $BINDIR/$1-$2-$ARCH
-	tar zcvf $BINDIR/$1-$2-$ARCH.tar.gz $BINDIR/$1-$2-$ARCH
-}
+apt-get install -y cabal-install pkg-config build-essential zlib1g-dev curl aria2
 
 CODE=`curl -s http://ip-api.com/json |tr ',' '\n' |grep "countryCode" |awk -F'"' '{print $4}'`
-
 cabal user-config init
-
 if [ "$CODE"x == "CN"x ];then
-	sed -i -r 's/hackage.haskell.org\//mirrors.tuna.tsinghua.edu.cn\/hackage/g' /root/.cabal/config
-	sed -i -r 's/hackage.haskell.org/mirrors.tuna.tsinghua.edu.cn/g' /root/.cabal/config
+	CABALDIR="/root/.cabal"
+	sed -i -r 's/hackage.haskell.org\//mirrors.tuna.tsinghua.edu.cn\/hackage/g' $CABALDIR/config
+	sed -i -r 's/hackage.haskell.org/mirrors.tuna.tsinghua.edu.cn/g' $CABALDIR/config
 fi
-
 cabal v2-update
 
 # download deps
@@ -40,36 +25,16 @@ for id in `cat deps.txt |grep -vE "#|^$"`;do
 	aria2c -x 16 "https://github.com/arm4rpi/pandoc-deps/releases/download/v0.1/$ARCH-$id.tar.gz"
 	tar zxvf $ARCH-$id.tar.gz
 done
-mv home/runner/.cabal/* /home/runner/.cabal
-ghc-pkg recache -v -f /home/runner/.cabal/store/ghc-8.6.5/package.db/
+mv home/runner/.cabal/* $CABALDIR
+ghc-pkg recache -v -f $CABALDIR/store/ghc-8.6.5/package.db/
 
-cabal v2-install pandoc-2.9.2 --flags="static embed_data_files" -v
+FLAGS='--flags="static embed_data_files"'
+echo "$PKG" |grep "citeproc" && FLAGS='--flags="static embed_data_files bibutils"'
+echo "$PKG" |grep "crossref" && FLAGS=''
 
-ls /home/runner/.cabal/store
-tar zcvf $ARCH-pandoc-2.9.2.tar.gz /home/runner/.cabal/store
+cabal v2-install $PKG $FLAGS -v
 
-<<COMMENT
-cd /root
-git clone https://github.com/jgm/pandoc
-cd pandoc
+ls $CABALDIR/store
 
-# cabal v2-build --dependencies-only . pandoc-citeproc
-cabal v2-build . pandoc-citeproc --flags="static embed_data_files bibutils -unicode_collation" --bindir=$BINDIR -v
-find /root -name "pandoc"
-find /root -name "pandoc-citeproc"
-cd ../
-
-git clone https://github.com/lierdakil/pandoc-crossref
-cd pandoc-crossref
-cabal v2-build . pandoc-crossref --flags="static" --bindir=$BINDIR
-find /root -name "pandoc-crossref"
-cd ../
-
-
-tag=`getTag "pandoc"`
-release "pandoc" "$tag"
-release "pandoc-citeproc" "$tag"
-
-tag=`getTag "pandoc-crossref"`
-release "pandoc-crossref" "$tag"
-COMMENT
+tar zcvf $ARCH-$PKG.tar.gz $CABALDIR/bin
+echo $PKG |grep -E "pandoc-[1-9]" && tar zcvf $ARCH-lib-$PKG.tar.gz $CABALDIR/store/ghc-8.6.5/$PKG-*
---
+++ ci.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+CIDIR=".github/workflows"
+HACKAGE="http://hackage.haskell.org/package"
+ITEMS=("pandoc" "pandoc-citeproc" "pandoc-crossref")
+
+[ ! -d $CIDIR ] && mkdir -p $CIDIR
+
+for id in ${ITEMS[@]};do
+	cat > $CIDIR/${id}.yml <<EOF
+name: $id 
+on: [push]
+jobs:
+EOF
+done
+
+function addJob() {
+	id=$1
+	arch=$2
+	qemuarch=aarch64
+	ubuntuarch=arm64
+	[ "$arch"x == "armv7l"x ] && qemuarch="arm" && ubuntuarch="armhf"
+
+	cat >> $CIDIR/${id}.yml <<EOF
+
+  $arch-$id:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: build
+      run: |
+        mkdir rootfs
+        export pkg=\`curl -s "$HACKAGE/$id" |grep "base href" |awk -F'"' '{print \$2}' |sed 's/\/$//g' |awk -F'/' '{print \$NF}'\`
+        curl -s -L "https://github.com/arm4rpi/pandoc-arm/releases/download/v0.1/$arch-\$pkg.tar.gz" -o rootfs/$arch-\$pkg.tar.gz
+        MIME=\`file -b --mime-type rootfs/$arch-\$pkg.tar.gz\`
+        echo \$MIME
+        [ "\$MIME"x == "application/gzip"x ] && echo "Already exists" && exit 0 || echo "Not exists"
+        sudo apt-get update
+        sudo apt-get install -y qemu-user-static aria2
+        aria2c -x 16 http://cdimage.ubuntu.com/ubuntu-base/releases/19.10/release/ubuntu-base-19.10-base-$ubuntuarch.tar.gz
+        cd rootfs
+        echo "decompression rootfs"
+        tar xvf ../ubuntu-base-19.10-base-$ubuntuarch.tar.gz &>/dev/null && echo "decompression rootfs successfull"
+        cp /usr/bin/qemu-$qemuarch-static usr/bin
+        cp /etc/resolv.conf etc
+        cp ../build.sh \$pkg
+        sudo mount -t devtmpfs devtmpfs dev
+        sudo mount -t devpts devpts dev/pts
+        sudo mount -t sysfs sysfs sys
+        sudo mount -t tmpfs tmpfs tmp
+        sudo mount -t proc proc proc
+        echo "chroot to arm"
+        sudo chroot . /\$pkg
+        echo "Upload Asset"
+        curl -H "Authorization: token \${{ secrets.TOKEN }}" -H "Content-Type: application/x-gzip" "https://uploads.github.com/repos/arm4rpi/pandoc-arm/releases/24024627/assets?name=$arch-\$pkg.tar.gz" --data-binary @$arch-\$pkg.tar.gz
+        echo "Upload $arch-lib-\$pkg"
+        [ -f $arch-lib-\$pkg.tar.gz ] && curl -H "Authorization: token \${{ secrets.TOKEN }}" -H "Content-Type: application/x-gzip" "https://uploads.github.com/repos/arm4rpi/pandoc-arm/releases/24024627/assets?name=$arch-lib-\$pkg.tar.gz" --data-binary @$arch-lib-\$pkg.tar.gz
+EOF
+}
+
+for id in ${ITEMS[@]};do
+	addJob $id aarch64
+	addJob $id armv7l
+done
